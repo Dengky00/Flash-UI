cc79e12894127d7a4ec3ba4d8b91d6c7914f24ca
Dengky00 <1605980369@qq.com>
2023-08-28 03:07:23 +0800

    让topnav是响应式的

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -10,11 +10,12 @@ const toggleAside = () => {
 
 <template>
     <div class="topnav">
-        <div class="logo" @click="toggleAside">LOGO</div>
+        <div class="logo">LOGO</div>
         <ul class="menu">
             <li>菜单1</li>
             <li>菜单2</li>
         </ul>
+        <span class="toggleAside" @click="toggleAside"></span>
     </div>
 </template>
 
@@ -31,13 +32,40 @@ const toggleAside = () => {
         margin-right: auto;
     }
 
+    >.toggleAside {
+        display: inline-block;
+        width: 24px;
+        height: 24px;
+        background: red;
+        position: absolute;
+        left: 16px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+
     >.menu {
         display: flex;
+        justify-content: center;
+        align-items: center;
         white-space: nowrap; //用于处理元素内的空白符:不会换行
         flex-wrap: nowrap;
 
         >li {
             margin: 0 1em;
+        }
+    }
+
+    @media (max-width:500px) {
+        >.menu {
+            display: none;
+        }
+
+        >.logo {
+            margin: 0 auto;
+        }
+
+        >.toggleAside {
+            display: inline-block;
         }
     }
 }
