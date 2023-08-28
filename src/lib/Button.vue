0038d280c68a247bc7e74f68b0afb79666bc3722
Dengky00 <1605980369@qq.com>
2023-08-28 20:15:04 +0800

    Button组件基础css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -18,4 +18,36 @@ export default {
     </button>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+$h: 32px;
+$border-color: #d9d9d9;
+$color: #333;
+$blue: #40a9ff;
+$radius: 4px;
+
+.flash-button {
+    box-sizing: border-box;
+    height: $h;
+    padding: 0 12px;
+    cursor: pointer;
+    display: inline-flex; //如果父元素不设置宽度,inline-flex父元素宽度等于子元素,flex父元素宽度100%
+    justify-content: center;
+    align-items: center;
+    white-space: nowrap;
+    background: white;
+    color: $color;
+    border: 1px solid $border-color;
+    border-radius: $radius;
+    box-shadow: 0 0 3px 0 fade-out(black, 0.8); //在给定的淡出程度下,fade-out会将颜色过渡为透明
+
+    &+& {
+        margin-left: 8px;
+    }
+
+    &:hover,
+    &:focus {
+        color: $blue;
+        border-color: $blue;
+    }
+}
+</style>
