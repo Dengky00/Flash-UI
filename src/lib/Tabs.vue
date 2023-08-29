f9b0a1959245d11192abb52e2db604792c9d2cef
Dengky00 <1605980369@qq.com>
2023-08-29 20:52:02 +0800

    Tabs css 1

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -15,10 +15,44 @@ const titles = defaults.map((element: VNode) => {
 </script>
 
 <template>
-    <div>
-        <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
-        <component v-for="(tab, index) in defaults" :key="index" :is="tab" />
+    <div class="flash-tabs">
+        <div class="flash-tabs-nav">
+            <div class="flash-tabs-nav-item" v-for="(t, index) in titles" :key="index">{{ t }}</div>
+        </div>
+        <div class="flash-tabs-content">
+            <component class="flash-tabs-content-item" v-for="(tab, index) in defaults" :key="index" :is="tab" />
+        </div>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+
+.flash-tabs {
+    &-nav {
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
+
+            &:first-child {
+                margin-left: 0;
+            }
+
+            &.selected {
+                color: $blue;
+            }
+        }
+    }
+
+    &-content {
+        padding: 8px 0;
+    }
+}
+</style>
