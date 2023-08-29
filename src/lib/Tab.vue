1b1f31650d01a03d18b5ee3351199448f074468b
Dengky00 <1605980369@qq.com>
2023-08-29 20:46:35 +0800

    Tabs嵌套渲染Tab

--- src/lib/Tab.vue
+++ src/lib/Tab.vue
@@ -1,8 +1,9 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+</script>
 
 <template>
     <div>
-        Tab 组件
+        <slot />
     </div>
 </template>
 
--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,8 +1,23 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import { VNode } from 'vue';
+import Tab from './Tab.vue'
+
+const slots = defineSlots();
+const defaults = slots.default()
+defaults.forEach((element: VNode) => {
+    if (element.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab')
+    }
+});
+const titles = defaults.map((element: VNode) => {
+    return element.props?.title
+})
+</script>
 
 <template>
     <div>
-        Tabs 组件
+        <div v-for="(t, index) in titles" :key="index">{{ t }}</div>
+        <component v-for="(tab, index) in defaults" :key="index" :is="tab" />
     </div>
 </template>
 
