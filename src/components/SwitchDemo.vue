1a8a55a6a7aebf288064b5f7ec94361956b5d337
Dengky00 <1605980369@qq.com>
2023-09-02 01:17:58 +0800

    安装prismjs高亮源码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -2,6 +2,9 @@
 import Switch1Demo from './Switch1.demo.vue'
 import Switch2Demo from './Switch2.demo.vue'
 import Button from '../lib/Button.vue'
+import 'prismjs';
+import 'prismjs/themes/prism-okaidia.css'
+const Prism = (window as any).Prism
 </script>
 
 <template>
@@ -16,7 +19,8 @@ import Button from '../lib/Button.vue'
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{ Switch1Demo.__sourceCode }}</pre>
+                <pre class="language-html"
+                    v-html="Prism.highlight(Switch1Demo.__sourceCode, Prism.languages.html, 'html')" />
             </div>
         </div>
         <div class="demo">
@@ -28,7 +32,8 @@ import Button from '../lib/Button.vue'
                 <Button>查看代码</Button>
             </div>
             <div class="demo-code">
-                <pre>{{ Switch2Demo.__sourceCode }}</pre>
+                <pre class="language-html"
+                    v-html="Prism.highlight(Switch2Demo.__sourceCode, Prism.languages.html, 'html')" />
             </div>
         </div>
     </div>
