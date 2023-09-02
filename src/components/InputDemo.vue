38092036a277ee2365a58e0705fd041a0e4fb80c
Dengky00 <1605980369@qq.com>
2023-09-02 18:36:50 +0800

    Input输入框组件

---
+++ src/components/InputDemo.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Input 组件示例</h1>
+    <Demo :component="InputDemo1" />
+    <Demo :component="InputDemo2" />
+    <Demo :component="InputDemo3" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import Demo from './Demo.vue'
+import InputDemo1 from './Input1.demo.vue';
+import InputDemo2 from './Input2.demo.vue';
+import InputDemo3 from './Input3.demo.vue';
+</script>
