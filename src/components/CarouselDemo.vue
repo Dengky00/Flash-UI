b2d370c49fdbe106f7265d8c7121d20bbd28ebfe
Dengky00 <1605980369@qq.com>
2023-09-02 18:36:24 +0800

    Carousel轮播图组件

---
+++ src/components/CarouselDemo.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1>Carousel 组件示例</h1>
+    <Demo :component="Carousel1Demo" />
+    <Demo :component="Carousel2Demo" />
+  </div>
+</template>
+
+<script lang="ts">
+// @ts-nocheck
+import Demo from './Demo.vue'
+import Carousel1Demo from './Carousel1.demo.vue'
+import Carousel2Demo from './Carousel2.demo.vue'
+
+export default {
+  components: { Demo },
+  setup() {
+    return { Carousel1Demo, Carousel2Demo }
+  },
+}
+</script>
